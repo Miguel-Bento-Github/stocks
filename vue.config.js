c6d939b3ca2111c7f81f809bf51795680a677d86
Miguel Bento <miguel.bento@eurail.com>
2020-10-13 22:17:51 +0200

    Inject sass variables through loader

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   devServer: {
     overlay: false,
   },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/_variables.scss";`,
+      },
+    },
+  },
 };
