760791491f232e21d332e94f7643323f9d3814f2
Miguel Bento <miguel.bento@eurail.com>
2020-10-13 09:46:35 +0200

    Implement store
    - Actions and mutations to replace stock datax

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,49 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import { Stock } from "../types";
 
-export default createStore({
-  state: {
+const state: State = {
+  stock: {
+    p: 0,
+    s: "",
+    t: 0,
+    v: 0,
+  },
+  time: [],
+  price: [],
+};
+
+const store = createStore({
+  state,
+  getters: {
+    time: (state) => state.time,
+    price: (state) => state.price,
   },
   mutations: {
+    setStock(state, stock: Stock) {
+      state.stock = stock;
+
+      const date = new Date(stock.t * 1000);
+      const minutes = date.getMinutes();
+      const time = `${date.getHours()}:${minutes > 9 ? minutes : "0" + minutes}:${date.getSeconds()}`;
+      const price = stock.p;
+
+      if (state.time.includes(time)) return;
+      state.time.push(time);
+      state.price.push(price);
+    },
   },
   actions: {
+    setStock: (context, stock: Stock) => {
+      context.commit("setStock", stock);
+    },
   },
-  modules: {
-  }
-})
+  plugins: [],
+});
+
+interface State {
+  stock: Stock;
+  time: string[];
+  price: number[];
+}
+
+export default store;
