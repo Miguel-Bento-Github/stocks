1d2f436db6092ca2ee4b375b6e9ad2c298f08789
Miguel Bento <miguel.bento@eurail.com>
2020-10-15 10:53:39 +0200

    Run linter

--- src/util/normaliseCasing.ts
+++ src/util/normaliseCasing.ts
@@ -3,8 +3,7 @@
  * @method normalise Will make first character of a string uppercase.
  */
 export default function normaliseCasing(input: string): string {
-  const isLowerCase = (string: string): boolean =>
-    string.valueOf().toLowerCase() === string.valueOf();
+  const isLowerCase = (string: string): boolean => string.valueOf().toLowerCase() === string.valueOf();
 
   const normalise = (string: string): string => {
     return `${string[0].toUpperCase()}${string.slice(1).toLowerCase()}`;
